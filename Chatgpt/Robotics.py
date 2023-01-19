61c42c5311985a1b9c403670cd972836423b20a7
Ramanan <38250400+RamananR-hash@users.noreply.github.com>
2023-01-19 09:08:30 +0530

    Add files via upload
    Chat GPT Based chatbot. Its awesome
     #chatbot #chatgpt #chatgptai #python #gtts #ai #openai #openaichatgpt 
    
    
    from gtts import gTTS #pip install gtts
    from playsound import playsound #pip install playsound
    import os
    import openai #pip install openai
    import speech_recognition as sr #pip install SpeechRecognition,pyaudio == 1.2.2

---
+++ Chatgpt/Robotics.py
@@ -0,0 +1,47 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+import openai
+import speech_recognition as sr
+r = sr.Recognizer()
+from time import sleep
+openai.api_key = "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"
+
+
+with sr.Microphone() as source:
+    print("Listening")
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    text = r.recognize_google(audio_data)
+    print(text)
+
+    
+prompt = text
+
+completions = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+message = completions.choices[0].text
+message=str(message.strip())
+
+
+print(message)
+tts=message
+
+language = 'en'  
+obj = gTTS(text=tts, lang=language, slow=False)   
+obj.save("say.mp3")   
+playsound("say.mp3")
+sleep(2)
+os.remove("say.mp3")
+try:
+    os.remove("say.mp3")
+except:
+    pass
+
