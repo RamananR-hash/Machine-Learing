c83ffe75f61a7cdaca2cbdf018a93b991b947c1c
Ramanan <38250400+RamananR-hash@users.noreply.github.com>
2023-02-15 07:44:21 +0530

    Update Chatbot.py

--- Chatgpt/Chatbot.py
+++ Chatgpt/Chatbot.py
@@ -4,7 +4,7 @@ import os
 import openai
 import speech_recognition as sr
 from time import sleep
-openai.api_key = "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"
+openai.api_key = ""#insert Your API HERE
 r = sr.Recognizer()
 while True:
     with sr.Microphone() as source:
@@ -18,8 +18,8 @@ while True:
 
 
     completions = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=text,
+        engine="text-davinci-002",#model
+        prompt=text,#text from microphone
         max_tokens=1024,
         n=1,
         stop=None,
@@ -27,18 +27,18 @@ while True:
     )
     message = completions.choices[0].text
     message=str(message.strip())
-    print(message)
+    print(message)#message output
 
 
     language = 'en'  
-    obj = gTTS(text=message, lang=language, slow=False)
+    obj = gTTS(text=message, lang=language, slow=False)#making as a audio
     print("Making Audio....")
     obj.save("say.mp3")
     print("Playing Audio....")
     playsound("say.mp3")
     sleep(2)
     print("Deleting Audio....")
-    os.remove("say.mp3")
+    os.remove("say.mp3")#deleting the audio
 
 
 
